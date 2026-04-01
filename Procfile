9880b4ec461a377347b51128fbc44bce3bf68154
Jacopo Fioretti <jpfiore13@gmail.com>
2026-04-01 21:53:24 +0200

    26/04/01

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn oltreilconfine.wsgi --log-file -
--- sito_oltreilconfine/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class SitoOltreilconfineConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sito_oltreilconfine'
-    verbose_name = 'Sito Oltreconfine'
